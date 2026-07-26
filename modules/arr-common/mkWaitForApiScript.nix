8351c6756a62a59e34f243ae19a1691a9b6b1dd0
Korimer <thekclov@gmail.com>
2026-07-26 15:20:29 -0600

    placeholder while i figure out where options are defined

--- modules/arr-common/mkWaitForApiScript.nix
+++ modules/arr-common/mkWaitForApiScript.nix
@@ -13,7 +13,7 @@ pkgs.writeShellScript "${serviceName}-wait-for-api" (
     BASE_URL="http://${serviceConfig.hostConfig.bindAddress}:${builtins.toString serviceConfig.hostConfig.port}${serviceConfig.hostConfig.urlBase}/api/${serviceConfig.apiVersion}"
 
     echo "Waiting for ${capitalizedName} API to be available..."
-    for i in {1..90}; do
+    for i in {1..${serviceConfig.waitForApiAttempts}}; do
       if ${
         mkSecureCurl serviceConfig.apiKey {
           url = "$BASE_URL/system/status";
@@ -23,11 +23,11 @@ pkgs.writeShellScript "${serviceName}-wait-for-api" (
         echo "${capitalizedName} API is available"
         exit 0
       fi
-      echo "Waiting for ${capitalizedName} API... ($i/90)"
-      sleep 1
+      echo "Waiting for ${capitalizedName} API... ($i/${serviceConfig.waitForApiAttempts})"
+      sleep ${serviceConfig.sleepOnFailSeconds}
     done
 
-    echo "${capitalizedName} API not available after 90 seconds" >&2
+    echo "${capitalizedName} API not available after ${serviceConfig.waitForApiAttempts * serviceConfig.sleepOnFailSeconds} seconds" >&2
     exit 1
   ''
 )
