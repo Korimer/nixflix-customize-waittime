988dce09baae9125a935d163391eedde8f12c0c9
Korimer <thekclov@gmail.com>
2026-07-26 23:43:05 -0600

    didnt realize you needed to use toString for this

--- modules/arr-common/mkWaitForApiScript.nix
+++ modules/arr-common/mkWaitForApiScript.nix
@@ -8,12 +8,14 @@ pkgs.writeShellScript "${serviceName}-wait-for-api" (
     mkSecureCurl = import ../../lib/mk-secure-curl.nix { inherit lib pkgs; };
     capitalizedName =
       lib.toUpper (builtins.substring 0 1 serviceName) + builtins.substring 1 (-1) serviceName;
+    apiAttempts = builtins.toString serviceConfig.waitForApiAttempts;
+    sleepSeconds = builtins.toString serviceConfig.sleepOnFailSeconds;
   in
   ''
     BASE_URL="http://${serviceConfig.hostConfig.bindAddress}:${builtins.toString serviceConfig.hostConfig.port}${serviceConfig.hostConfig.urlBase}/api/${serviceConfig.apiVersion}"
 
     echo "Waiting for ${capitalizedName} API to be available..."
-    for i in {1..${serviceConfig.waitForApiAttempts}}; do
+    for i in {1..${apiAttempts}}; do
       if ${
         mkSecureCurl serviceConfig.apiKey {
           url = "$BASE_URL/system/status";
@@ -23,11 +25,11 @@ pkgs.writeShellScript "${serviceName}-wait-for-api" (
         echo "${capitalizedName} API is available"
         exit 0
       fi
-      echo "Waiting for ${capitalizedName} API... ($i/${serviceConfig.waitForApiAttempts})"
-      sleep ${serviceConfig.sleepOnFailSeconds}
+      echo "Waiting for ${capitalizedName} API... ($i/${apiAttempts})"
+      sleep ${sleepSeconds}
     done
 
-    echo "${capitalizedName} API not available after ${serviceConfig.waitForApiAttempts * serviceConfig.sleepOnFailSeconds} seconds" >&2
+    echo "${capitalizedName} API not available after ${apiAttempts * sleepSeconds} seconds" >&2
     exit 1
   ''
 )
